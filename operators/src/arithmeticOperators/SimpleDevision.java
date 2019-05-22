a63ac45a75fa696324ee128b8930bbe74e9725cf
one-lightning <lightning@thunderstorm.pro>
2019-05-22 22:57:28 +0300

    [#9] Arithmetic operators / SimpleDevision

---
+++ operators/src/arithmeticOperators/SimpleDevision.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  22/05/2019   
+ * Exercise: #9
+ * Purpose:  Arithmetic operators / SimpleDevision
+ */
+
+package arithmeticOperators;
+
+public class SimpleDevision {
+
+	public static void main(String[] args) {
+		int aNumber = 98;
+		int bNumber = 2;
+		int resultNumber = 0;
+		
+		resultNumber = (aNumber / bNumber);
+		System.out.println("Dividing," + aNumber + "/" + bNumber + "=" + resultNumber);
+
+	}
+
+}
