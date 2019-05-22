268aadefae2637d49200f84ec3fa58e2ad7357ff
one-lightning <lightning@thunderstorm.pro>
2019-05-22 20:49:20 +0300

    [#9] Arithmetic operators / Bank Debt

---
+++ operators/src/arithmeticOperators/BankDebt.java
@@ -0,0 +1,32 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  22/05/2019   
+ * Exercise: #9
+ * Purpose:  Arithmetic operators / Bank Debt 
+ */
+
+package arithmeticOperators;
+
+public class BankDebt {
+
+	public static void main(String[] args) {
+
+		int bankAccountDebt = -200;
+		int offshoreSupplyIncome = 32;
+		bankAccountDebt = (bankAccountDebt + offshoreSupplyIncome);
+		System.out.println("Current bank account debt :" + bankAccountDebt);
+		bankAccountDebt = (bankAccountDebt + offshoreSupplyIncome);
+		System.out.println("Current bank account debt :" + bankAccountDebt);
+		bankAccountDebt = (bankAccountDebt + offshoreSupplyIncome);
+		System.out.println("Current bank account debt :" + bankAccountDebt);
+		bankAccountDebt = (bankAccountDebt + offshoreSupplyIncome);
+		System.out.println("Current bank account debt :" + bankAccountDebt);
+		bankAccountDebt = (bankAccountDebt + offshoreSupplyIncome);
+		System.out.println("Current bank account debt :" + bankAccountDebt);
+		bankAccountDebt = (bankAccountDebt + offshoreSupplyIncome);
+		System.out.println("Current bank account debt :" + bankAccountDebt);
+		bankAccountDebt = (bankAccountDebt + offshoreSupplyIncome);
+		System.out.println("Current bank account debt :" + bankAccountDebt);
+	}
+
+}
