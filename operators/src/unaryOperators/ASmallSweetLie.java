24a8859de939d9e78414288579d42750529c1323
one-lightning <lightning@thunderstorm.pro>
2019-05-23 19:27:05 +0300

    [#10] Unary operators / A small sweet lie

---
+++ operators/src/unaryOperators/ASmallSweetLie.java
@@ -0,0 +1,18 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  23/05/2019   
+ * Exercise: #10
+ * Purpose:  Unary operators / A small sweet lie
+ */
+
+package unaryOperators;
+
+public class ASmallSweetLie {
+
+	public static void main(String[] args) {
+		boolean isJonAtTheOffice = false;
+		System.out.println("It's" + " "  + !isJonAtTheOffice + ", I think I saw him today.");
+
+	}
+
+}
