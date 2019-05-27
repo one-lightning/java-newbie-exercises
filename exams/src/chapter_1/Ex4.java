5ff82a147797a67a2bbc6630193c39ffcc4d9d40
one-lightning <lightning@thunderstorm.pro>
2019-05-28 01:28:19 +0300

    [#20]Using 1 variable of type int and 2 variables of type short write a program that will output:``` 32222 + 32222 = %output% ```

---
+++ exams/src/chapter_1/Ex4.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  28/05/2019   
+ * Exercise: #20
+ * Purpose: Using 1 variable of type int and 2 variables of type short write a program that will output:``` 32222 + 32222 = %output% ```
+ */
+
+package chapter_1;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		
+		short addition = 32222;
+		short var = 32222;
+		int output = 64444;
+		
+		System.out.println("```\n" + addition + " + " + var + "=" + " " + output + "\n```");
+
+	}
+
+}
