08b28494d21f803dd37988acdcc4ef0b095db468
one-lightning <lightning@thunderstorm.pro>
2019-05-27 10:56:38 +0300

    [#15] Advanced assignment Operators / Bank Terminal

--- operators/src/advancedassignmentOperators/BankTerminal.java
+++ operators/src/advancedassignmentOperators/BankTerminal.java
@@ -1,10 +1,9 @@
 /**
  * Name: Cristina Moloman    
- * Date:  26/05/2019   
+ * Date:  27/05/2019   
  * Exercise: #15
  * Purpose:  Advanced assignment Operators / Bank Terminal
  */
-
 
 package advancedassignmentOperators;
 
@@ -14,21 +13,30 @@ public class BankTerminal {
 		short extractedId = 0;
 		int extractedAmount = 0;
 		int remainingBalance = 364;
-		
+
 		extractedId = 1;
 		extractedAmount = 30;
 		remainingBalance -= extractedAmount;
-		System.out.println("Extracted amount: " + extractedAmount + " " + " Current balance: " + remainingBalance);
-		
+		System.out.println("--" + "[" + "#" + extractedId + "]" + "----------------------");
+		System.out.println("-" + " Extracted amount: " + extractedAmount + "($)" + ". " + "-");
+		System.out.println("-" + " Current balance: " + remainingBalance + "($)" + ". " + "-");
+		System.out.println("----------------------------");
+
 		extractedId = 2;
 		extractedAmount = 100;
 		remainingBalance -= extractedAmount;
-		System.out.println("Extracted amount: " + extractedAmount + " " + " Current balance: " + remainingBalance);
-		
+		System.out.println("--" + "[" + "#" + extractedId + "]" + "----------------------");
+		System.out.println("-" + " Extracted amount: " + extractedAmount + "($)" + "." + "-");
+		System.out.println("-" + " Current balance: " + remainingBalance + "($)" + ". " + "-");
+		System.out.println("----------------------------");
+
 		extractedId = 3;
 		extractedAmount = 70;
 		remainingBalance -= extractedAmount;
-		System.out.println("Extracted amount: " + extractedAmount + " " + " Current balance: " + remainingBalance);
+		System.out.println("--" + "[" + "#" + extractedId + "]" + "----------------------");
+		System.out.println("-" + " Extracted amount: " + extractedAmount + "($)" + ". " + "-");
+		System.out.println("-" + " Current balance: " + remainingBalance + "($)" + ". " + "-");
+		System.out.println("----------------------------");
 
 	}
 
