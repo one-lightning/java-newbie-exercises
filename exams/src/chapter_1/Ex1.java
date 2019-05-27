a87e73270433a1f1919038192280e9a6d3698a48
one-lightning <lightning@thunderstorm.pro>
2019-05-27 21:58:13 +0300

    [#20] Exam (Operators, variables, primitive datatypes) / Using 3 variables of type int, byte short create a program that will output to the console:

---
+++ exams/src/chapter_1/Ex1.java
@@ -0,0 +1,27 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  27/05/2019   
+ * Exercise: #20
+ * Purpose:  Exam (Operators, variables, primitive datatypes) / Using 3 variables of type int, byte short create a program that will output to the console:
+ */
+
+
+package chapter_1;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		int ID = 7889233;
+		short Followers = 32761;
+		byte Age = 122;
+		
+		System.out.println("================");
+		System.out.println("ID:" + " " + ID);
+		System.out.println("Followers:" + " " + Followers);
+		System.out.println("Age:" + " " + Age);
+		System.out.println("================");
+
+	}
+
+}
