ac58240005ebcfe74b39b6355209bf2b94a92485
one-lightning <lightning@thunderstorm.pro>
2019-05-28 06:36:45 +0300

    [#20]Using only 1 variable of type int output the following without using assignment operator:Countdown: 10
    Countdown: 9
    Countdown: 8
    Countdown: 7
    Countdown: 6
    ...
    Countdown: -2

---
+++ exams/src/chapter_1/Ex7.java
@@ -0,0 +1,45 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  28/05/2019   
+ * Exercise: #20
+ * Purpose: Using only 1 variable of type int output the following without using assignment operator:
+ * ```
+ *  
+ */
+package chapter_1;
+
+public class Ex7 {
+
+	public static void main(String[] args) {
+		
+		short Countdown=10;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		Countdown--;
+		System.out.println("Countdown:" + " " + Countdown);
+		
+
+	}
+
+}
