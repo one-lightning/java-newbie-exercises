f0cde84dfc14a567653e5dc056892490e35d9364
one-lightning <lightning@thunderstorm.pro>
2019-06-01 18:55:08 +0300

    [#25] Flow control/PartyInvitation

---
+++ flowcontrol/src/conditional/PartyInvitation.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  01/06/2019   
+ * Exercise: #25
+ * Purpose:  Flow control/PartyInvitation
+ */
+
+package conditional;
+
+public class PartyInvitation {
+
+	public static void main(String[] args) {
+		boolean isAccepted;
+		isAccepted = true;
+		if (isAccepted) {
+			System.out.println("Ok let's go to the party!");
+		} else {
+			System.out.println("Not today babe, not today!");
+		}
+	}
+
+}
