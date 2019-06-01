c1e6a1c1aab055387fe4e0241bc6c3f6b9776251
one-lightning <lightning@thunderstorm.pro>
2019-06-01 18:53:34 +0300

    [#25] Flow control/WorkMeeting

---
+++ flowcontrol/src/conditional/WorkMeeting.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  01/06/2019   
+ * Exercise: #25
+ * Purpose:  Flow control/WorkMeeting
+ */
+
+package conditional;
+
+public class WorkMeeting {
+
+	public static void main(String[] args) {
+		short currentDay = 25;
+		short scheduledDay = 25;
+		if ((currentDay == scheduledDay)) {
+			System.out.println("Ok gotta go to the meeting!");
+		}
+		if ((currentDay != scheduledDay)) {
+			System.out.println("Not today babe!");
+		}
+	}
+}
