efd9867ebca82118db98b338d97392377dc6d910
one-lightning <lightning@thunderstorm.pro>
2019-06-02 20:06:27 +0300

    [#25] Flow control/TreeHeight

---
+++ flowcontrol/src/conditional/TreeHeight.java
@@ -0,0 +1,31 @@
+/**
+ * Name: Cristina Moloman    
+ * Date:  02/06/2019   
+ * Exercise: #25
+ * Purpose:  Flow control/TreeHeight
+ */
+
+package conditional;
+
+public class TreeHeight {
+
+	public static void main(String[] args) {
+		int treeHeightInMeters = 1;
+		if ((treeHeightInMeters >= 40)) {
+			System.out.println("COLOSSAL TREE!");
+		} else if (treeHeightInMeters >= 30) {
+			System.out.println("GIGANTIC TREE!");
+		} else if (treeHeightInMeters >= 20) {
+			System.out.println("WHAT A TALL TREE!");
+		} else if (treeHeightInMeters >= 10) {
+			System.out.println("Wow this is very old tree!");
+		} else if (treeHeightInMeters >= 10) {
+			System.out.println("Wow this is very old tree!");
+		} else if (treeHeightInMeters >= 5) {
+			System.out.println("Beautifull grownup tree!");
+		} else if (treeHeightInMeters >= 1) {
+			System.out.println("Small little sapling))");
+		}
+	}
+
+}
